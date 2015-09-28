e0466c837d25285cc113ac4566bc61e2be3d87c0
dn200978lak <dn200978lak@gmail.com>
2015-09-28 19:02:57 +0300

    release

---
+++ src/main/java/com/algorithm/A.java
@@ -0,0 +1,10 @@
+package main.java.com.algorithm;
+
+/**
+ * Created by alexandr on 28.09.15.
+ */
+public class A {
+
+    synchronized void method() { }
+
+}
---
+++ src/main/java/com/algorithm/B.java
@@ -0,0 +1,12 @@
+package main.java.com.algorithm;
+
+/**
+ * Created by alexandr on 28.09.15.
+ */
+public class B {
+
+    void method() {
+        synchronized (this) { }
+    }
+
+}
